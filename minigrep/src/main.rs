31c3a49b5618bb5da9c20af9ea31572bb3e8e225
shi.kun <shi.kun@ivis.co.jp>
2026-03-05 09:39:10 +0900

    Commit Tutorail Message

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,8 +4,9 @@ use minigrep::Config;
 
 fn main() {
 
-    let args :Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // let args :Vec<String> = env::args().collect();
+    let args = env::args();
+    let config = Config::new(args).unwrap_or_else(|err| {
         eprintln!("\x1b[31mProblem parsing arguments: {}\x1b[0m", err);
         process::exit(1);
     });
